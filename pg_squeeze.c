fc49f597a24576816d58b398ffa660f68741814a
umgfoin <umgfoin@users.noreply.github.com>
2024-06-14 11:45:24 +0200

    Revert "PG <= 14 misses PGDLLIMPORT on some variables" Approach taken here won't be feasable for windows builds: Symbol redeclaration with different storage class is not allowed. See commit df765641 which fixes this for windows-builds. This reverts commit 3c24e6fad7870930e5db18af74bb16114cb31be1.

--- pg_squeeze.c
+++ pg_squeeze.c
@@ -57,11 +57,6 @@
 #include "utils/rel.h"
 #include "utils/syscache.h"
 
-#if PG_VERSION_NUM < 150000
-extern PGDLLIMPORT int wal_segment_size;
-extern PGDLLIMPORT bool FirstSnapshotSet;
-#endif
-
 #if PG_VERSION_NUM < 120000
 #error "PostgreSQL version 12 or higher is required"
 #endif
